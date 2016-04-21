dda05ff70a9f4d8385357615438db5299ba432d1
Charles "Demurgos" Samborski <demurgos.net@gmail.com>
2016-04-21 23:31:53 +0200

    Use conventional spinal-case for filenames

--- src/api.ts
+++ src/api.ts
@@ -4,60 +4,60 @@ import {setOptions} from "./options";
 import {Api, ApiContext, ApiIO} from "./interfaces/api";
 
 // Method generators
-import getAddUserToGroup from "./api/addUserToGroup";
-import getChangeArchivedStatus from "./api/changeArchivedStatus";
-import getChangeGroupImage from "./api/changeGroupImage";
-import getChangeNickname from "./api/changeNickname";
-import getChangeThreadColor from "./api/changeThreadColor";
-import getChangeThreadEmoji from "./api/changeThreadEmoji";
-import getDeleteMessage from "./api/deleteMessage";
-import getDeleteThread from "./api/deleteThread";
-import getGetCurrentUserID from "./api/getCurrentUserID";
-import getGetFriendsList from "./api/getFriendsList";
-import getGetOnlineUsers from "./api/getOnlineUsers";
-import getGetThreadHistory from "./api/getThreadHistory";
-import getGetThreadInfo from "./api/getThreadInfo";
-import getGetThreadList from "./api/getThreadList";
-import getGetUserID from "./api/getUserID";
-import getGetUserInfo from "./api/getUserInfo";
+import getAddUserToGroup from "./api/add-user-to-group";
+import getChangeArchivedStatus from "./api/change-archived-status";
+import getChangeGroupImage from "./api/change-group-image";
+import getChangeNickname from "./api/change-nickname";
+import getChangeThreadColor from "./api/change-thread-color";
+import getChangeThreadEmoji from "./api/change-thread-emoji";
+import getDeleteMessage from "./api/delete-message";
+import getDeleteThread from "./api/delete-thread";
+import getGetCurrentUserID from "./api/get-current-user-id";
+import getGetFriendsList from "./api/get-friends-list";
+import getGetOnlineUsers from "./api/get-online-users";
+import getGetThreadHistory from "./api/get-thread-history";
+import getGetThreadInfo from "./api/get-thread-info";
+import getGetThreadList from "./api/get-thread-list";
+import getGetUserID from "./api/get-user-id";
+import getGetUserInfo from "./api/get-user-info";
 import getListen from "./api/listen";
 import getLogout from "./api/logout";
-import getMarkAsRead from "./api/markAsRead";
-import getRemoveUserFromGroup from "./api/removeUserFromGroup";
-import getSearchForThread from "./api/searchForThread";
-import getSendMessage from "./api/sendMessage";
-import getSendTypingIndicator from "./api/sendTypingIndicator";
-import getSetTitle from "./api/setTitle";
+import getMarkAsRead from "./api/mark-as-read";
+import getRemoveUserFromGroup from "./api/remove-user-from-group";
+import getSearchForThread from "./api/search-for-thread";
+import getSendMessage from "./api/send-message";
+import getSendTypingIndicator from "./api/send-typing-indicator";
+import getSetTitle from "./api/set-title";
 
 function getApi (defaultFuncs: ApiIO, api: Api, ctx: ApiContext): Api {
-  return {
-    setOptions: api.setOptions,
-    getAppState: api.getAppState,
-    addUserToGroup: getAddUserToGroup(defaultFuncs, api, ctx),
-    changeArchivedStatus: getChangeArchivedStatus(defaultFuncs, api, ctx),
-    changeGroupImage: getChangeGroupImage(defaultFuncs, api, ctx),
-    changeNickname: getChangeNickname(defaultFuncs, api, ctx),
-    changeThreadColor: getChangeThreadColor(defaultFuncs, api, ctx),
-    changeThreadEmoji: getChangeThreadEmoji(defaultFuncs, api, ctx),
-    deleteMessage: getDeleteMessage(defaultFuncs, api, ctx),
-    deleteThread: getDeleteThread(defaultFuncs, api, ctx),
-    getCurrentUserID: getGetCurrentUserID(defaultFuncs, api, ctx),
-    getFriendsList: getGetFriendsList(defaultFuncs, api, ctx),
-    getOnlineUsers: getGetOnlineUsers(defaultFuncs, api, ctx),
-    getThreadHistory: getGetThreadHistory(defaultFuncs, api, ctx),
-    getThreadInfo: getGetThreadInfo(defaultFuncs, api, ctx),
-    getThreadList: getGetThreadList(defaultFuncs, api, ctx),
-    getUserID: getGetUserID(defaultFuncs, api, ctx),
-    getUserInfo: getGetUserInfo(defaultFuncs, api, ctx),
-    listen: getListen(defaultFuncs, api, ctx),
-    logout: getLogout(defaultFuncs, api, ctx),
-    markAsRead: getMarkAsRead(defaultFuncs, api, ctx),
-    removeUserFromGroup: getRemoveUserFromGroup(defaultFuncs, api, ctx),
-    searchForThread: getSearchForThread(defaultFuncs, api, ctx),
-    sendMessage: getSendMessage(defaultFuncs, api, ctx),
-    sendTypingIndicator: getSendTypingIndicator(defaultFuncs, api, ctx),
-    setTitle: getSetTitle(defaultFuncs, api, ctx),
-  }
+  // api.setOptions;
+  // api.getAppState;
+  api.addUserToGroup = getAddUserToGroup(defaultFuncs, api, ctx);
+  api.changeArchivedStatus = getChangeArchivedStatus(defaultFuncs, api, ctx);
+  api.changeGroupImage = getChangeGroupImage(defaultFuncs, api, ctx);
+  api.changeNickname = getChangeNickname(defaultFuncs, api, ctx);
+  api.changeThreadColor = getChangeThreadColor(defaultFuncs, api, ctx);
+  api.changeThreadEmoji = getChangeThreadEmoji(defaultFuncs, api, ctx);
+  api.deleteMessage = getDeleteMessage(defaultFuncs, api, ctx);
+  api.deleteThread = getDeleteThread(defaultFuncs, api, ctx);
+  api.getCurrentUserID = getGetCurrentUserID(defaultFuncs, api, ctx);
+  api.getFriendsList = getGetFriendsList(defaultFuncs, api, ctx);
+  api.getOnlineUsers = getGetOnlineUsers(defaultFuncs, api, ctx);
+  api.getThreadHistory = getGetThreadHistory(defaultFuncs, api, ctx);
+  api.getThreadInfo = getGetThreadInfo(defaultFuncs, api, ctx);
+  api.getThreadList = getGetThreadList(defaultFuncs, api, ctx);
+  api.getUserID = getGetUserID(defaultFuncs, api, ctx);
+  api.getUserInfo = getGetUserInfo(defaultFuncs, api, ctx);
+  api.listen = getListen(defaultFuncs, api, ctx);
+  api.logout = getLogout(defaultFuncs, api, ctx);
+  api.markAsRead = getMarkAsRead(defaultFuncs, api, ctx);
+  api.removeUserFromGroup = getRemoveUserFromGroup(defaultFuncs, api, ctx);
+  api.searchForThread = getSearchForThread(defaultFuncs, api, ctx);
+  api.sendMessage = getSendMessage(defaultFuncs, api, ctx);
+  api.sendTypingIndicator = getSendTypingIndicator(defaultFuncs, api, ctx);
+  api.setTitle = getSetTitle(defaultFuncs, api, ctx);
+
+  return api;
 }
 
 export function buildAPI(globalOptions, html, jar): [ApiContext, ApiIO, Api] {
